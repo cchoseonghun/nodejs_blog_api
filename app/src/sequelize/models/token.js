8ac475bb69883ac0c34da51b8f7c7f058bb4fe14
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2022-12-21 16:15:37 +0900

    feat: user와 token FK 종속 추가가

--- app/src/sequelize/models/token.js
+++ app/src/sequelize/models/token.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      models.token.belongsTo(models.user, {foreignKey: 'userId'})
     }
   }
   Token.init({
